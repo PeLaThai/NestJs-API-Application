8dd6ebe97774fdce37607f20276ba7ed26f3b381
PeLaiahT <135104101+PeLaThai@users.noreply.github.com>
2023-07-13 16:49:02 +0700

    Add User Module + Authen with Passport Js

---
+++ src/user/user.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './models/user.model';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { AuthController } from './controllers/auth.controller';
+import { UserRepository } from './repository/user.repository';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('SECRETKEY'),
+        signOptions: {
+          expiresIn: configService.get('EXPIRESIN'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [UserService, AuthService, UserRepository, JwtStrategy],
+})
+export class UserModule {}
